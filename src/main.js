e70bb12848fbdf074737a100f6438f26c8107cfa
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-01-30 00:34:35 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
+Vue.mixin({
+  created: function () {
+    this.gsap = gsap;
+  },
+});
 createApp(App).mount("#app");
