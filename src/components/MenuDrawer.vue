cb696e24e89608662e69dc62e1254e6dfdd4da40
Greez <imgreez@protonmail.com>
2022-02-18 19:11:30 +0330

    menu bar added

--- src/components/MenuDrawer.vue
+++ src/components/MenuDrawer.vue
@@ -13,11 +13,42 @@
       class="drawer"
       color="#64B5F6"
     >
-      <div class="h-full w-full p-6 flex flex-col justify-start align-center">
+      <div
+        class="p-6 flex border-b-2 border-mainYellow flex-row w-full justify-around"
+      >
+        <DefaultButton
+          @click="bar = 'menu'"
+          class="mx-4 p-4 hover:bg-mainYellow"
+          >منو</DefaultButton
+        >
+        <DefaultButton
+          @click="bar = 'catagories'"
+          class="hover:bg-mainYellow p-4"
+          >دسته بندی</DefaultButton
+        >
+      </div>
+      <div
+        class="h-full w-full p-6 flex flex-col justify-start align-center"
+        :class="{ hidden: bar !== 'menu' }"
+      >
         <DefaultButton class=" ">خونه</DefaultButton>
         <DefaultButton class=" ">خرید</DefaultButton>
         <DefaultButton class="text-2xl">درباره ی پانکی مانکی</DefaultButton>
         <DefaultButton class=" ">ادمین</DefaultButton>
+      </div>
+
+      <div
+        class="h-full w-full p-6 flex flex-col justify-start align-center"
+        :class="{ hidden: bar !== 'catagories' }"
+      >
+        <DefaultButton class=" ">هودی</DefaultButton>
+        <DefaultButton class=" ">آکسسوری</DefaultButton>
+        <DefaultButton class="text-2xl">تیشرت</DefaultButton>
+        <DefaultButton class=" ">کلاه</DefaultButton>
+         <DefaultButton class=" ">پیراهن</DefaultButton>
+        <DefaultButton class=" ">کفش</DefaultButton>
+        <DefaultButton class="text-2xl">دیزاین</DefaultButton>
+        <DefaultButton class=" ">بول</DefaultButton>
       </div>
     </v-navigation-drawer>
   </div>
@@ -33,6 +64,7 @@ export default {
   data() {
     return {
       drawer: false,
+      bar: "menu",
     };
   },
 };
