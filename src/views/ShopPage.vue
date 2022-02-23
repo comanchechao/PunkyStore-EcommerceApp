420283149bb68247deafff1a0154476806fa6cf0
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-02-23 04:01:48 +0330

    Added ListBox Components

--- src/views/ShopPage.vue
+++ src/views/ShopPage.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="main" class="w-screen flex justify-center align-center p-6">
-    <div class="w-full h-screen bg-darkPurple"></div>
+    <div class="w-full h-screen background rounded-b-xl">
+      <div class="w-full h-20 flex justify-center align-center bg-mainPink">
+        <ListBox />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import ListBox from "../components/ListBox.vue";
+export default {
+  components: {
+    ListBox,
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.background {
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  background-color: white;
+}
+</style>
