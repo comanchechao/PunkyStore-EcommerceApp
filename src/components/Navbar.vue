26d9bd213de1b38b95b096e7f3827eb92558743e
comanchechao <61477751+comanchechao@users.noreply.github.com>
2022-04-07 02:05:52 +0330

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,16 +18,7 @@
         </DefaultButton>
       </router-link>
     </div>
-    <router-link to="/shop">
-      <DefaultButton class="text-white">
-        <v-icon>mdi-shopping</v-icon>
-      </DefaultButton>
-    </router-link>
-    <router-link to="/">
-      <DefaultButton class="text-white">
-        <v-icon>mdi-home</v-icon>
-      </DefaultButton>
-    </router-link>
+
     <router-link to="/admin">
       <DefaultButton class="text-white">
         <v-icon>mdi-account-cowboy-hat</v-icon>
