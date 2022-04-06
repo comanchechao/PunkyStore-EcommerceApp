0c7bf54ca6a30f9654ee055284f0de5718736cc6
Greez <imgreez@protonmail.com>
2022-04-06 10:29:01 +0330

    continue from the previous commit

--- src/components/Adminastration.vue
+++ src/components/Adminastration.vue
@@ -142,7 +142,7 @@
                   class="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
                   @click="closeModal"
                 >
-                  Got it, thanks!
+                  بستن
                 </button>
               </div>
             </div>
--- src/views/CheckoutPage.vue
+++ src/views/CheckoutPage.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="flex flex-col w-screen h-full bg-Sky-500 overflow-hidden" >
+  <div class="flex flex-col w-screen h-full bg-Sky-500 overflow-hidden">
     <div class="w-full flex mt-20 justify-center align-center content">
       <DefaultButton
-      class="m-2 px-6 py-4 rounded bg-Cyan-700 text-white" @click="component = 'CheckoutSubmit'">
+        class="m-2 px-6 py-4 rounded bg-Cyan-700 text-white"
+        @click="component = 'CheckoutSubmit'"
+      >
         مرحله بعدی
       </DefaultButton>
 
       <DefaultButton
-      class="m-2 px-6 py-4 rounded bg-Cyan-700 text-white" @click="component = 'CheckoutInfo'">
+        class="m-2 px-6 py-4 rounded bg-Cyan-700 text-white"
+        @click="component = 'CheckoutInfo'"
+      >
         مرحله قبلی
       </DefaultButton>
     </div>
-    <transition  @enter="enter" @leave="leave" name="route" appear>
-      <component :is="component"></component>
-    </transition>
-
-    <div></div>
+    <div class="mb-8">
+      <transition @enter="enter" @leave="leave" name="route" appear>
+        <component :is="component"></component>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -30,17 +34,14 @@ export default {
   components: { CheckoutInfo, DefaultButton, CheckoutSubmit },
   setup() {
     const component = ref("CheckoutInfo");
-    
 
-
-    
     const enter = (el, done) => {
       const tl = gsap.timeline({
         onComplete: done,
       });
       tl.set(el, {
         autoAlpha: 0,
-        opacity: 0 ,
+        opacity: 0,
         x: 400,
         transformOrigin: "100% ",
       });
@@ -63,7 +64,7 @@ export default {
         {
           autoAlpha: 0,
           x: 400,
-          opacity: 0 ,
+          opacity: 0,
           duration: 0.5,
           ease: "Power2.easeOut",
           onComplete: done,
