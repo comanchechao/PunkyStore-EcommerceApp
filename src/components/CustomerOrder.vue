07cef1ca3c3ec4553b7976a79195d4527347f5fa
Greez <imgreez@protonmail.com>
2022-04-29 11:58:24 +0330

    add product (in progress) & customer orders added to manamgement

--- src/components/CustomerOrder.vue
+++ src/components/CustomerOrder.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-row-reverse  justify-around w-full h-16 text-gray-900 border-b-2 hover:bg-Sky-100 hover:border-b-4 transition transform p-1 rounded my-1 border-white align-center"
+    class="flex flex-row-reverse justify-around w-full h-16 text-gray-900 border-b-2 hover:bg-Sky-100 hover:border-b-4 transition transform p-1 rounded my-1 border-white align-center"
   >
     <div
       :class="{
@@ -25,7 +25,7 @@
       <h1
         class="self-center align-center transition ease-in duration-200 hover:text-white"
       >
-        نام کاربری
+        {{ order.fullname }}
       </h1>
     </div>
     <div class="customerInfo">
@@ -55,20 +55,71 @@
         </template>
 
         <template #modalBody>
-          <radio-group>
-            <template #itemTitle>
-              کمل
-            </template>
-
-            <template #itemDetail>
-             <div class="flex w-full justify-around">
-                <span>2555 تومان </span>
-               <span>تعداد : 1 </span>
-                <span>رنگ: آبی </span>
-                 <span>سایز: xl </span>
-             </div>
-            </template>
-          </radio-group>
+          <RadioGroup>
+            <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
+            <div class="space-y-2 text-right" v-if="items">
+              <RadioGroupOption
+                as="template"
+                v-for="item in items[0].cart_items[0]"
+                :key="item.id"
+                v-slot="{ active, checked }"
+              >
+                <div
+                  :class="[
+                    active
+                      ? 'ring-2 ring-offset-2 ring-offset-Sky-300 ring-white ring-opacity-60'
+                      : '',
+                    checked
+                      ? 'bg-sky-900 bg-opacity-75 text-white '
+                      : 'bg-white ',
+                  ]"
+                  class="relative transition-all flex px-5 py-4 rounded-lg shadow-md cursor-pointer focus:outline-none"
+                >
+                  <div
+                    class="flex flex-row-reverse items-center justify-between w-full"
+                  >
+                    <div class="flex items-center">
+                      <div class="text-sm">
+                        <RadioGroupLabel
+                          as="p"
+                          :class="checked ? 'text-white' : 'text-gray-900'"
+                          class="font-medium"
+                        >
+                          {{ item.item.title }}
+                        </RadioGroupLabel>
+                        <RadioGroupDescription
+                          as="div"
+                          :class="checked ? 'text-white' : 'text-gray-500'"
+                          class="flex justify-around"
+                        >
+                          {{ item.item.description }}
+                          <span aria-hidden="true"> &middot; </span>
+                        </RadioGroupDescription>
+                      </div>
+                    </div>
+                    <div v-show="checked" class="flex-shrink-0 text-white">
+                      <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none">
+                        <circle
+                          cx="12"
+                          cy="12"
+                          r="12"
+                          fill="#fff"
+                          fill-opacity="0.2"
+                        />
+                        <path
+                          d="M7 13l3 3 7-7"
+                          stroke="#fff"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </RadioGroupOption>
+            </div>
+          </RadioGroup>
         </template>
       </modal>
     </div>
@@ -100,22 +151,44 @@
         </template>
 
         <template #modalBody>
-          <div class="flex flex-col align-center justify-between p-5 bg-gray-50 rounded-lg divide-solid space-y-2 mt-5">
-            <div class="flex flex-row-reverse border-b-2 my-1 justify-between w-full">
+          <div
+            class="flex flex-col align-center justify-between p-5 bg-gray-50 rounded-lg divide-solid space-y-2 mt-5"
+          >
+            <div
+              class="flex flex-row-reverse border-b-2 my-1 justify-between w-full"
+            >
               <span>نام کاربری</span>
-              <span>بهمیکس</span>
+              <span>{{ order.fullname }}</span>
             </div>
-              <div class="flex flex-row-reverse border-b-2 my-1 justify-between w-full">
+            <div
+              class="flex flex-row-reverse border-b-2 my-1 justify-between w-full"
+            >
               <span>شماره تلفن</span>
-              <span>3442333</span>
+              <span>{{ order.phone_number }}</span>
             </div>
-              <div class="flex flex-row-reverse border-b-2 my-1 justify-between w-full">
+            <div
+              class="flex flex-row-reverse border-b-2 my-1 justify-between w-full"
+            >
               <span>شهر </span>
-              <span>بهمیکس</span>
+              <span>{{ order.city }}</span>
             </div>
-              <div class="flex flex-row-reverse border-b-2 my-1 justify-between w-full">
+            <div
+              class="flex flex-row-reverse border-b-2 my-1 justify-between w-full"
+            >
               <span>استان</span>
-              <span>بهمیکسلند</span>
+              <span>{{ order.province }}</span>
+            </div>
+            <div
+              class="flex flex-row-reverse border-b-2 my-1 justify-between w-full"
+            >
+              <span>استان</span>
+              <span>{{ order.full_address }}</span>
+            </div>
+            <div
+              class="flex flex-row-reverse border-b-2 my-1 justify-between w-full"
+            >
+              <span>استان</span>
+              <span>{{ order.email }}</span>
             </div>
           </div>
         </template>
@@ -126,13 +199,23 @@
 
 <script>
 import modal from "../components/Modal.vue";
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import {
+  Menu,
+  MenuButton,
+  MenuItems,
+  MenuItem,
+  RadioGroup,
+  RadioGroupLabel,
+  RadioGroupDescription,
+  RadioGroupOption,
+} from "@headlessui/vue";
 import { EyeIcon, TrashIcon } from "@heroicons/vue/solid";
 import DefaultButton from "./DefaultButton.vue";
-import { computed, onMounted } from "vue";
-import RadioGroup from '../components/RadioGroup.vue'
+import { computed, onMounted, ref } from "vue";
+import { supabase } from "../supabase";
+
 export default {
-  props: ["tab"],
+  props: ["tab", "order"],
   components: {
     modal,
     Menu,
@@ -142,18 +225,44 @@ export default {
     EyeIcon,
     TrashIcon,
     DefaultButton,
-    RadioGroup
+    RadioGroup,
+    RadioGroupLabel,
+    RadioGroupDescription,
+    RadioGroupOption,
   },
   setup(props) {
+    const items = ref([]);
     const displayedTab = computed(() => {
       return props.tab;
     });
 
+    const orderDetailId = computed(() => {
+      return props.order.id;
+    });
+
     onMounted(() => {
+      getOrderItems();
       console.log(displayedTab.value);
     });
 
-    return { displayedTab };
+    async function getOrderItems() {
+      try {
+        const { data, error } = await supabase
+          .from("order_items")
+          .select()
+          .eq("order_detail_id", orderDetailId.value);
+        // .eq("product-category", props.category.title);
+
+        if (error) throw error;
+        items.value = data;
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        console.log(items.value[0].cart_items[0]);
+      }
+    }
+
+    return { displayedTab, items };
   },
 };
 </script>
