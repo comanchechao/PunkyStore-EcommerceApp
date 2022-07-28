c6a07b3775777476e186a3668e8857f6feef6c15
Greez <imgreez@protonmail.com>
2022-07-28 11:23:37 +0330

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+require("dotenv").config()
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
